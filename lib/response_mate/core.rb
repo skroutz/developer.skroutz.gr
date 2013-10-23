e1304f78576ebaf881ad580f5b15cbd69659182a
Dimitris Zorbas <zorbash@skroutz.gr>
2013-10-23 16:51:34 +0300

    Make ResponseMate configurable
    Configuration Example ->
    https://github.com/skroutz/apiv3/wiki/ResponseMate-Design-Document#configure

---
+++ lib/response_mate/core.rb
@@ -0,0 +1,19 @@
+module ResponseMate
+  class << self
+    attr_accessor :configuration
+  end
+
+  class Configuration
+    attr_accessor :output_dir, :requests_manifest
+
+    def initialize
+      @output_dir = '../../output/responses/'
+      @requests_manifest = '../../requests.yml'
+    end
+  end
+
+  def self.setup
+    self.configuration ||= Configuration.new
+    yield(configuration) if block_given?
+  end
+end
