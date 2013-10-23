de4a776ff89f635c4e1086fee0f806c8e068f144
Dimitris Zorbas <zorbash@skroutz.gr>
2013-10-23 19:14:55 +0300

    Make ResponseMate::Formatter expose a method for status + body

--- lib/response_mate/formatter.rb
+++ lib/response_mate/formatter.rb
@@ -1,8 +1,17 @@
 module ResponseMate
   module Formatter
+    def render_recording(key)
+      recording = load_recording key
+      format_status(recording) + format_body(recording)
+    end
+
     def render_recording_body(key); format_body load_recording(key) end
 
     private
+    def format_status(recording)
+      %(<pre class="headers"><code>Status: #{recording[:status]}</code></pre>\n)
+    end
+
     def format_body(recording)
       %(<pre class="highlight"><code class="language-javascript">%s</code></pre>) %
         JSON.pretty_generate(JSON.parse(recording[:body]))
