fb1f2b4a768020d0080453e4ac2bf69bc0577e90
Dimitris Zorbas <zorbash@skroutz.gr>
2014-02-04 17:35:24 +0200

    Add toggler for responses

--- output/shared/js/documentation.js
+++ output/shared/js/documentation.js
@@ -8,4 +8,16 @@ $(function() {
 
     window.scrollTo(0, $(this.href.match(/(#.+)/)[0]).offset().top)
   });
+
+  $('.toggler').on('click', function(e) {
+    var $el = $(e.currentTarget),
+        $target = $el.siblings('.response').toggle();
+    if($el.data('open') || $el.data('open') == undefined) {
+      $el.text('hide example');
+      $el.data('open', false);
+    } else {
+      $el.text('view example');
+      $el.data('open', true);
+    }
+  });
 });
