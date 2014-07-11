dea7defb0fc95bfff9be2525642bcdd2b6196266
Chrisovalantis Kefalidis <valantis@skroutz.gr>
2014-07-11 16:56:50 +0300

    Port missing method render_code

--- lib/code_example_helper.rb
+++ lib/code_example_helper.rb
@@ -1,10 +1,14 @@
 module CodeExampleHelper
+  def render_code(snippet, language)
+    code language do
+      snippet
+    end
+  end
+
   def render_code_from_file(filename)
     content = YAML.load_file(File.expand_path(File.join('./resources',
                                                         'code_examples',
                                                         "#{filename}.yml")))
-    code content[:language] do
-      content[:code]
-    end
+    render_code content[:code], content[:language]
   end
 end
