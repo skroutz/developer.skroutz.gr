00f888e80e364e8eebbb8a5437a953c2ec9b90dd
Dimitris Zorbas <zorbash@skroutz.gr>
2014-07-14 12:51:36 +0300

    Migrate per section sidebars
    To declare the sidebar to be used for a pattern of pages (eg /api/v3/*)
    you have to edit config.rb and define sidebar metadata as in the
    following example:
    
            page: 'awesome_section/pages/*', sidebar: 'partials/sidebars/awesome'

--- config.rb
+++ config.rb
@@ -1,6 +1,12 @@
 require 'lib/code_example_helper'
 
-page 'index.html', layout: false
+page 'index.html', layout: false, sidebar: 'koko'
+
+page 'api/v3/*', sidebar: 'partials/sidebars/api/v3'
+page 'guides/*', sidebar: 'partials/sidebars/guides'
+page 'authentication/*', sidebar: 'partials/sidebars/authentication'
+page 'analytics/*', sidebar: 'partials/sidebars/analytics'
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
