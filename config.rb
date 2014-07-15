d705e09d40de2cde5d887ba61d633390c8c587b3
Dimitris Zorbas <zorbash@skroutz.gr>
2014-07-15 15:54:32 +0300

    Remove comments from config.rb

--- config.rb
+++ config.rb
@@ -24,21 +24,6 @@ activate :syntax, line_numbers: false
 helpers CodeExampleHelper
 helpers ResponseMateHelper
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :markdown, layout_engine: :erb, toc_levels: '2'
 
 set :css_dir, 'stylesheets'
@@ -49,18 +34,4 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
