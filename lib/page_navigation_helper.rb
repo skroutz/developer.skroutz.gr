319100ccc1471addd57bdd76a450d9af53c61208
Chrisovalantis Kefalidis <valantis@skroutz.gr>
2014-08-01 14:05:33 +0300

    Serve on-demand multiple locales for individual pages and flavor
    To make a single page available in other locales for the current
    flavor, you have to define it in the frontmatter:
    
      locale: { some_flavor: ['en', 'el'], another_flavor: ['en', 'tr'] }
    
    EXAMPLE
    
    The `feedspec` page is available in 3 languages: ['en', 'el', 'tr'].
    
    For the `skroutz` flavor, the `feedspec` page should be available in
    ['en', 'el'].
    For the `alve` flavor, the `feedspec` page should be available in
    ['en', 'tr'].
    
    To achieve it, we have to declare at the frontmatter of:
    
    - `feedspec.en.html.erb`
      locale: { alve: ['en', 'tr'], skroutz: ['en', 'el'] }
    
    - `feedspec.tr.html.erb`
      locale: { alve: ['en', 'tr'] }
    
    - `feedspec.el.html.erb`
      locale: { skroutz: ['en', 'el'] }

--- lib/page_navigation_helper.rb
+++ lib/page_navigation_helper.rb
@@ -62,12 +62,116 @@ module PageNavigationHelper
   def sidebar_child_link(doc_key, doc, page)
     page_url = url_to_doc_page(doc.base, page)
 
-    html = "<a class='nav-link #{active_child_link(page_url)}' "
+    html = "<a class='navbar-link #{active_child_link(page_url)}' "
     html << "href='#{page_url}'>"
     html << t("docs.#{doc_key}.#{page.title}",
               flavor: settings.site_name.capitalize)
     html << "</a>"
 
     html
+  end
+
+  # Builds a set of <span> elements for a given list of items.
+  #
+  # @param [Array] items the array of items
+  # @param [String] classes the CSS classes to style the list
+  # @return [String] the constructed <span> elements
+  def bs_text(items, classes)
+    html = "<span class='#{classes}'>"
+
+    items.each do |item|
+      html << '<span'
+      html << ' class="active"' if item[:active]
+      html << '>'
+      html << item[:html]
+      html << '</span>'
+    end
+
+    html << '</span>'
+    html
+  end
+
+  # Builds a Bootstrap List component for a given list of items.
+  #
+  # @param [Array] list_items the array of items
+  # @param [String] list_classes the CSS classes to style the list
+  # @return [String] the constructed <ul/> element
+  def bs_list(list_items, list_classes)
+    html = "<ul class='#{list_classes}'>"
+
+    list_items.each do |list_item|
+      html << '<li'
+      html << ' class="active"' if list_item[:active]
+      html << '>'
+      html << list_item[:html]
+      html << '</li>'
+    end
+
+    html << '</ul>'
+    html
+  end
+
+  # Builds a Bootstrap Dropdown component for a given list of items.
+  #
+  # @param [Array] list_items the array of items
+  # @param [String] list_classes the CSS classes to style the list
+  # @param [String] dropdown_classes the CSS classes to style the dropdown
+  # @param [String] toggle the dropdown toggle content
+  # @return [String] the constructed <ul/> element
+  def bs_dropdown(list_items, list_classes, dropdown_classes, toggle)
+    html = "<ul class='nav navbar-nav #{list_classes}'>"
+    html << "<li class='dropdown #{dropdown_classes}'>"
+    html << '<a class="dropdown-toggle" data-toggle="dropdown" href="#">'
+    html << toggle
+    html << '</a>'
+    html << '<ul class="dropdown-menu" role="menu">'
+
+    list_items.each do |list_item|
+      html << '<li'
+      html << ' class="active"' if list_item[:active]
+      html << '>'
+      html << list_item[:html]
+      html << '</li>'
+    end
+
+    html << '</ul></li></ul>'
+    html
+  end
+
+  # Builds an array of hashes with the available locales for a given page.
+  #
+  # @param [Hash] page the page to build the array for
+  # @return [Array] the array list
+  def locale_links(page)
+    return [] if page.data.locale.nil?
+
+    list_items = Array.new
+
+    page.data.locale[flavor].each do |lang|
+      active = (lang == I18n.locale.to_s)
+
+      if lang == I18n.default_locale.to_s
+        if lang == I18n.locale.to_s
+          url = page.url
+        else
+          url = page.url.gsub("/#{I18n.locale}/", '/')
+        end
+      else
+        if I18n.locale == I18n.default_locale
+          url = "/#{lang}#{page.url}"
+        else
+          url = page.url.gsub("/#{I18n.locale}/", "/#{lang}/")
+        end
+      end
+
+      html = "<a class='nav-link' href='#{url}'>"
+      html << "<span class='lang-code'>#{lang}</span>"
+      html << "<span class='lang-name'>#{t('lang.'+lang)}</span>"
+      html << '</a>'
+
+      list_items << { html: html, active: active }
+    end
+
+    list_items
   end
 end
