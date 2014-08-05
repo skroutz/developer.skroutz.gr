35dacc5c17a63bbd1a17d5a23efeb544d5b152a5
Fotos Georgiadis <fotos@skroutz.gr>
2014-08-05 13:25:39 +0300

    Dynamically generate featured in front page

--- lib/document_helper.rb
+++ lib/document_helper.rb
@@ -7,7 +7,24 @@ module DocumentHelper
     doc.deprecated == true
   end
 
+  def featured?(doc)
+    doc.featured == true
+  end
+
   def flavored?(doc)
     doc.flavors.blank? || (doc.flavors.present? && doc.flavors.include?(flavor))
+  end
+
+  def featured
+    data.docs.select do |_, doc|
+      featured?(doc) && !deprecated?(doc) && flavored?(doc)
+    end
+  end
+
+  def featured_offset(size)
+    sm = size > 3 ? 0 : 3 - size
+    md = size > 6 ? 0 : 6 - size
+
+    "col-sm-offset-#{sm} col-md-offset-#{md}"
   end
 end
