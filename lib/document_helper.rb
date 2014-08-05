c8c403966c55c38951b01339cc7bad1d1b519351
Fotos Georgiadis <fotos@skroutz.gr>
2014-08-05 13:23:53 +0300

    Move deprecated? helper to DocumentHelper

--- lib/document_helper.rb
+++ lib/document_helper.rb
@@ -1,4 +1,12 @@
 module DocumentHelper
+  # Checks if a given Doc is deprecated
+  #
+  # @param [Hash] doc the Doc to check
+  # @return [Boolean] true for deprecated Doc, otherwise false
+  def deprecated?(doc)
+    doc.deprecated == true
+  end
+
   def flavored?(doc)
     doc.flavors.blank? || (doc.flavors.present? && doc.flavors.include?(flavor))
   end
